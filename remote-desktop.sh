5ab8b71a0ac72c938fcd65494d54991e1e322cba
Khanh nine seven <33818840+khanh97dev@users.noreply.github.com>
2024-08-29 09:48:40 +0700

    Update remote-desktop.sh

--- remote-desktop.sh
+++ remote-desktop.sh
@@ -4,14 +4,14 @@ docker run -d \
   --cap-add=NET_ADMIN \
   --device /dev/net/tun \
   --name=rdesktop \
-  --security-opt seccomp=unconfined `#optional` \
+  --security-opt seccomp=unconfined \
   -e PUID=1000 \
   -e PGID=1000 \
   -e TZ=Etc/UTC \
   -p 3389:3389 \
-  -v /var/run/docker.sock:/var/run/docker.sock `#optional` \
-  -v /rdesktop/data:/config `#optional` \
-  --device /dev/dri:/dev/dri `#optional` \
-  --shm-size="1gb" `#optional` \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /rdesktop/data:/config \
+  --device /dev/dri:/dev/dri \
+  --shm-size="1gb" \
   --restart unless-stopped \
   lscr.io/linuxserver/rdesktop:latest
